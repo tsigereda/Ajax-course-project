1afb6daed7f676f8eb9562fe4914837c39d6e5e6
Tsigereda <tsigeredasfw@gmail.com>
2017-09-02 21:59:08 +0200

    ajax call to nyt

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ function loadData() {
     var $greeting = $('#greeting');
 
     // clear out old data before new request
-    // $wikiElem.text("");
-    // $nytElem.text("");
+    $wikiElem.text("");
+    $nytElem.text("");
 
     // load streetview
 var street = $('#street').val();
@@ -22,10 +22,26 @@ console.log(requestUrl);
 
 $body.append("<img class='bgimg' src="+ requestUrl +"></img>");
 
-console.log(street);
-console.log(city);
-console.log(requestUrl);
-    // YOUR CODE GOES HERE!
+    // YOUR CODE GOES HERE!'
+
+    ////NYT
+
+    var url = "https://api.noytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({'api-key': "c10b75e5bbd94b5490fc6e17f7f4979a", 'q': street });
+
+$.getJSON(url, function(data){
+    console.log(data);
+    
+    data.response.docs.forEach(function(element) {
+        $nytElem.append("<li>"+element.snippet+"</li>");
+          console.log(element.snippet);
+
+    }, this);
+}).error(function(){
+    $nytElem.text("can not be loaded!!!");
+}
+
+);
 
     return false;
 };
